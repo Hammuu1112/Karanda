254a9f90594f273c0e367cf10e51d261bf2a0079
HwanSangYeonHwa <97436524+HwanSangYeonHwa@users.noreply.github.com>
2022-06-20 12:16:25 +0900

    Add files via upload

--- install.js
+++ install.js
@@ -10,6 +10,23 @@ window.addEventListener('beforeinstallprompt', (event) => {
     // Remove the 'hidden' class from the install button container.
     divInstall.classList.toggle('hidden', false);
     divInstall.style.display = 'block';
+
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        // The deferred prompt isn't available.
+        return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log('👍', 'userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    divInstall.classList.toggle('hidden', true);
+}
 });
 
 async function select() {
