2d2598896947065da052b2eda85d3c1e8486f456
HwanSangYeonHwa <97436524+HwanSangYeonHwa@users.noreply.github.com>
2023-10-14 17:07:44 +0900

    hotfix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -81,10 +81,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "e34ceecb1135bb043ec4fe3caf3b04ba
 "icons/favicon-32x32.png": "bf8ae1f0210a568b65cc2d4208004cdd",
 "icons/favicon.ico": "a4a2e9bd7f38cd26922a4ad9ac9a8714",
 "img/flutter-logo.png": "680596443602745483f66870e07d7e94",
-"index.html": "e75a813bf41c4a98abe0ea29b3a4e9a2",
-"/": "e75a813bf41c4a98abe0ea29b3a4e9a2",
+"index.html": "a785d3121e794c5cd5fbb4e363789e19",
+"/": "a785d3121e794c5cd5fbb4e363789e19",
 "install.js": "2c8700fd8bf96f556c179c5eaea8e8a2",
-"main.dart.js": "4c28cb89ebe679554a26ba6a17eba1e5",
+"main.dart.js": "f08088620b60958369a8100e94eef67f",
 "manifest.json": "cee3ff988c33441e88211224b05a88fb",
 "styles.css": "163855fd240b2a8d4e378c5187267607",
 "version.json": "97820017695a18a7d6c1ba49dbbe6582"};
